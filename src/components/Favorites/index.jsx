09fa60fa37bd30ec9e3e7c07daf24d92615c109e
DanielSerenoPD <danielsereno432s@gmail.com>
2021-09-16 18:44:22 -0500

    Modification in the Favorites component

--- src/components/Favorites/index.jsx
+++ src/components/Favorites/index.jsx
@@ -14,27 +14,6 @@ export default function Favorites() {
       <div className = {style.listFavorites}>
       <List title = "Favorites" animes = {animes} />
       </div>
-      { animes&&animes.map(anime=>
-      anime.mal_id===""?
-      <CardDescription key={anime.mal_id}
-          title={anime.title}
-          type={anime.type}
-          episodes={anime.episodes}
-          aired={anime.aired}
-          duration={anime.duration}
-          rating={anime.rating}
-          score={anime.score}
-          rank={anime.rank}
-          popularity={anime.popularity}
-          premiered={anime.premiered}
-          opening_themes={anime.opening_themes}
-          ending_themes={anime.ending_themes}
-        />
-        :
-        null
-      )
-      }
-      
     </div>
   );
 }
